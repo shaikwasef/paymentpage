548a1fc0fc7782779595117a59cf62ee84572dec
shaik wasef <wasefshaik@gmail.com>
2021-05-22 00:25:15 +0530

    initial changes

---
+++ src/components/ViewComponents/ViewContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ViewContainer.css'
+
+function ViewContainer(props) {
+    return (
+        <div className="view-container">
+            Most Popular
+        </div>
+    );
+}
+
+export default ViewContainer;
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: ['Mulish',
+        'sans-serif'].join(',')
+       }
+})
+
+export default theme;
