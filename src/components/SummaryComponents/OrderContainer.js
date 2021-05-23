5a4b705c3a6b90b11abd6afed1901da944aa35ac
shaik wasef <wasefshaik@gmail.com>
2021-05-23 17:08:16 +0530

    deleted tables

--- src/components/SummaryComponents/OrderContainer.js
+++ src/components/SummaryComponents/OrderContainer.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 import './OrderContainer.css'
-import ListContainer from './OrderContainerComponents/ListContainer';
+
 
 function OrderContainer(props) {
     return (
         <div className="order-container">
-            <ListContainer/>
             <div>hi</div>
         </div>
     );
